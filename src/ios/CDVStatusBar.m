0ce40efd359a80939fb6afbc6babc3ca7e853960
Etienne Lemay <etienne@heliom.ca>
2021-05-26 16:07:15 -0400

    Always set frame.origin.y to 0 when `statusBarOverlaysWebView` App should handle safe area. This fixes white bar on iPhone max devices with zoomed display.

--- src/ios/CDVStatusBar.m
+++ src/ios/CDVStatusBar.m
@@ -446,20 +446,7 @@ static const void *kStatusBarStyle = &kStatusBarStyle;
     if (!self.statusBarOverlaysWebView) {
         frame.origin.y = height;
     } else {
-        frame.origin.y = height >= 20 ? height - 20 : 0;
-        if (isIOS11) {
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000
-            if (@available(iOS 11.0, *)) {
-                float safeAreaTop = self.webView.safeAreaInsets.top;
-                if (height >= safeAreaTop && safeAreaTop >0) {
-                    // Sometimes when in-call/recording/hotspot larger status bar is present, the safeAreaTop is 40 but we want frame.origin.y to be 20
-                    frame.origin.y = safeAreaTop == 40 ? 20 : height - safeAreaTop;
-                } else {
-                    frame.origin.y = 0;
-                }
-            }
-#endif
-        }
+        frame.origin.y = 0;
     }
     frame.size.height -= frame.origin.y;
     self.webView.frame = frame;
